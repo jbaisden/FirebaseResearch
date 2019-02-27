ff7cbfac27b92fc336aea6bd890193c0b9a806a2
jbaisden <jasonbaisden@gmail.com>
2019-02-27 12:37:09 -0600

    added a WORKING Fire Store example

---
+++ FireStoreExample/FireStore-ToDo-Demo/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, 'my firestore to do demo'),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
